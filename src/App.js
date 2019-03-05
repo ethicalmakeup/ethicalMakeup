aa10340f1d65ebbb024f8f9e786f26dbe88045bd
Julie <julie.bam1985@gmail.com>
2019-03-05 14:53:08 -0500

    made an axios call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  
+  componentDidMount(){
+    axios({
+      method: 'get',
+      url: 'http://makeup-api.herokuapp.com/api/v1/products.json',
+      responseType: 'json',
+      params: {
+        format: 'json',
+        product_tags: 'vegan'
+      }
+      
+
+    }).then(response => {
+      console.log(response);
+    })
+  }
+  
+  
   render() {
     return (
       <div className="App">
