69067bebb348894ba81d10a613c3228978e7c3a4
James <james83lau@gmail.com>
2019-03-06 16:25:05 -0500

    adding handleClick to Details component

---
+++ src/components/Details/Details.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+const Details = (props) => {
+	return (
+		<section>
+			<div>
+				<img></img>
+			</div>
+			<div>
+				<h2 />
+				<p />
+				<p />
+				<p />
+				<a href="" />
+			</div>
+		</section>
+	);
+};
+
+export default Details;
