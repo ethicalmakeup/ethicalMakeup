669f8208e25019ee4028c225d1d1856d90ac47d5
Luke <mackenzieluke94@gmail.com>
2019-03-08 16:55:58 -0500

    added 2 missing images and made minor style changes

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Gallery.css';
 import Clearfix from 'react-clearfix';
+import image1 from './image1.jpg';
+import image2 from './image2.jpg';
 
 const Gallery = (props) => {
     return (
@@ -9,7 +11,9 @@ const Gallery = (props) => {
                 {props.userResults.map(result => {
                     return (
                         <li key={result.id} onClick={() => {props.handleClick(result.id)}}>
-                            <img src={result.image_link} alt={result.name} />
+                            {(result.id === 1043) ? (<img src={image1} alt={result.name} />)
+                            : (result.id === 1042) ? <img src={image2} alt={result.name} />
+                            : <img src={result.image_link} alt={result.name}/>}
                             <h2>{result.name}</h2>
                             {result.price > 0 ? (<p>${result.price}</p>) : (<p>Price not available</p>)}
                         </li> 
