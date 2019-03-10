c26c443d034f3a7aebc7bdaf61298aa887b63cba
James <james83lau@gmail.com>
2019-03-10 14:18:39 -0400

    committing before pull from master

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Gallery.css';
 import Clearfix from 'react-clearfix';
 
+
 const Gallery = (props) => {
     return (
         <section>
@@ -9,7 +10,7 @@ const Gallery = (props) => {
                 {props.userResults.map(result => {
                     return (
                         <li key={result.id} onClick={() => {props.handleClick(result.id)}}>
-                            <img src={result.image_link} alt={result.name} />
+                            <img src={result.image_link} alt={result.name}/>
                             <h2>{result.name}</h2>
                             {result.price > 0 ? (<p>${result.price}</p>) : (<p>Price not available</p>)}
                         </li> 
