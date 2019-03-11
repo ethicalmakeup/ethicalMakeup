ebc4bbe8e4a925a7cb3c4ffc5cd18b8542f5714f
Luke <mackenzieluke94@gmail.com>
2019-03-11 18:45:32 -0400

    just so many changes

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -7,7 +7,7 @@ import image2 from '../Gallery/image2.jpg';
 
 const Details = (props) => {
 	return (
-        <div className='description-card' key={props.chosenProductObject.id}>
+        <div className='description-card clearfix wrapper' key={props.chosenProductObject.id}>
             <div className='image'>
                 {props.chosenProductObject.id === 1043 ? 
                     ( <img src={image1} alt={props.chosenProductObject.name} />)
@@ -15,26 +15,29 @@ const Details = (props) => {
                         ( <img src={image2} alt={props.chosenProductObject.name} /> ) 
                         :( <img src={props.chosenProductObject.image_link} alt={props.chosenProductObject.name}/>)}
             </div>
+            <Clearfix />
             <div className='description'>
                 <div>
                     <h2>{props.chosenProductObject.name}</h2>
-                    <p>${props.chosenProductObject.price}</p>
-                    <p>{props.chosenProductObject.description}</p>
+                    {props.chosenProductObject.price > 0 ? (<p>${props.chosenProductObject.price}</p>) : (<p className="none">Price is unavailable</p>)}
+                    
+                    <p className="descriptionText">{props.chosenProductObject.description}</p>
                     {props.chosenProductObject.rating ? (<p>Rating: {props.chosenProductObject.rating}/5</p>):(<p>Rating: Not available</p>)}
-                    <a href={props.chosenProductObject.product_link} target="_blank" rel="noopener noreferrer">Buy Now</a>
+                    <div className="clearfix">
+                        {props.chosenProductObject.product_colors.map(color => {
+                            return (
+                                <div className='colours'>
+                                    <span className='colour' style={{ background: color.hex_value }}></span>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <a href={props.chosenProductObject.product_link} target="_blank" rel="noopener noreferrer" className="button">Buy Now</a>
                 </div>
-                <div>
-                    {props.chosenProductObject.product_colors.map(color => {
-                        return (
-                            <div className='colors'>
-                                <p className='colour-name'>{color.colour_name}</p>
-                                <span className='colour' style={{background: color.hex_value}}></span>
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
+
+                
             <Clearfix />
+            </div>
         </div>
     )
 }
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './Landing.css';
-import { Link, animateScroll as scroll } from 'react-scroll';
-
 
 const Landing = (props) => {
     return (
