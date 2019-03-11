b541972982ca98e618b343c3239aa5462ec2591b
James <james83lau@gmail.com>
2019-03-10 23:34:49 -0400

    styled galllery section, modified favicon

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -4,10 +4,14 @@ import Clearfix from 'react-clearfix';
 import image1 from './image1.jpg';
 import image2 from './image2.jpg';
 
+const round = (price) => {
+    return Number.parseFloat(price).toFixed(2);
+  }
+
 
 const Gallery = (props) => {
     return (
-        <section>
+        <section className="gallery">
             <ul>
                 {props.userResults.map(result => {
                     return (
@@ -15,8 +19,8 @@ const Gallery = (props) => {
                             {(result.id === 1043) ? (<img src={image1} alt={result.name} />)
                             : (result.id === 1042) ? <img src={image2} alt={result.name} />
                             : <img src={result.image_link} alt={result.name}/>}
-                            <h2>{result.name}</h2>
-                            {result.price > 0 ? (<p>${result.price}</p>) : (<p>Price not available</p>)}
+                            <h2 className="title">{result.name}</h2>
+                            {result.price > 0 ? (<p className="price">${result.price}</p>) : (<p className="none">Price is unavailable</p>)}
                         </li> 
                     )
                 })}
